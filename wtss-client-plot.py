e8dfd63d57a0091adf96a8eeb95c59cbf4bd27d7
abner <abiner.anjos@gmail.com>
2020-01-22 14:46:44 -0300

    fixed-errors-on-url

--- wtss-client-plot.py
+++ wtss-client-plot.py
@@ -6,7 +6,7 @@ from wtss import wtss
 w = wtss("http://brazildatacube.dpi.inpe.br")
 
 # retrieve the time series for location with longitude = -54, latitude =  -12
-ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date="2018-01-01", end_date="2018-12-31")
+ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date = "2018-01-01", end_date = "2018-12-31")
 
 fig, ax = pyplot.subplots()
 
--- wtss-client.py
+++ wtss-client.py
@@ -11,7 +11,7 @@ cv_scheme = w.describe_coverage("C64m:MEDIAN")
 
 print(cv_scheme)
 
-ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date="2018-01-01", end_date="2018-12-31")
+ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date = "2018-01-01", end_date = "2018-12-31")
 
 print(ts["red"])
 print(ts["nir"])
