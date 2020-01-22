be6ad0ee1642d6e188e76cc78de577c5af9fb279
abner <abiner.anjos@gmail.com>
2020-01-22 14:45:20 -0300

    fixed-errors-on-url

--- wtss-client-plot.py
+++ wtss-client-plot.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as pyplot
 import matplotlib.dates as mdates
 from wtss import wtss
 
-# w = wtss("http://www.dpi.inpe.br/tws")
-w = wtss("http://localhost:5000/wtss")
+# w = wtss("http://localhost:5000")
+w = wtss("http://brazildatacube.dpi.inpe.br")
 
 # retrieve the time series for location with longitude = -54, latitude =  -12
-ts = w.time_series("mod13q1_512", "red", -12.0, -54.0, start_date="2001-01-01", end_date="2001-12-31")
+ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date="2018-01-01", end_date="2018-12-31")
 
 fig, ax = pyplot.subplots()
 
--- wtss-client.py
+++ wtss-client.py
@@ -1,17 +1,17 @@
 from wtss import wtss
 
-# w = wtss("http://www.dpi.inpe.br/tws")
-w = wtss("http://localhost:5000/wtss/tws")
+# w = wtss("http://localhost:5000")
+w = wtss("http://brazildatacube.dpi.inpe.br")
 
 cv_list = w.list_coverages()
 
 print(cv_list)
 
-cv_scheme = w.describe_coverage("mod13q1_512")
+cv_scheme = w.describe_coverage("C64m:MEDIAN")
 
 print(cv_scheme)
 
-ts = w.time_series("mod13q1_512", ("red", "nir"), -12.0, -54.0, "", "")
+ts = w.time_series("C64m:MEDIAN", "red", -12.0, -54.0, start_date="2018-01-01", end_date="2018-12-31")
 
 print(ts["red"])
 print(ts["nir"])
